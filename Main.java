3dbbf726fc30ed52cd8baa0cf3f0b17cdde58980
nipplelion <piolotorrico@yahoo.com>
2022-05-07 12:05:00 -0700

    Move menu display to TaskView

--- Main.java
+++ Main.java
@@ -1,6 +1,3 @@
-import java.util.NoSuchElementException;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
 
@@ -9,60 +6,6 @@ public class Main {
 
         TaskController controller = new TaskController(model, view);
 
-        menu();
-    }
-
-    public static void menu() {
-        char response = 'a';
-        Scanner sc = new Scanner(System.in);
-
-        while (true) {
-            System.out.println("Welcome to CS3560 PSS! Enter a response at the prompt or press CTRL + X + C to quit.");
-            System.out.println();
-            System.out.println("1: View Schedule");
-            System.out.println("2: Edit Schedule");
-            System.out.println("3: Write To File");
-            System.out.println("4: Read From File");
-            System.out.println();
-
-            try {
-                response = sc.next().charAt(0);
-            } catch (NoSuchElementException e) {
-                System.out.println();
-                System.out.println("Goodbye!");
-                break;
-            }
-
-            switch (response) {
-                case '1':
-                    //display schedule
-                    System.out.println();
-                    System.out.println("View Schedule");
-                    break;
-
-                case '2':
-                    //edit schedule
-                    System.out.println();
-                    System.out.println("Edit Schedule");
-                    break;
-
-                case '3':
-                    //write schedule to file
-                    System.out.println();
-                    System.out.println("Write To File");
-                    break;
-
-                case '4':
-                    //read schedule from file
-                    System.out.println();
-                    System.out.println("Read From File");
-                    break;
-
-                default:
-                    System.out.println("Invalid response. Please enter a valid response.");
-                    break;
-            }
-            System.out.println();
-        }
+        view.displayMenu();
     }
 }
--- TaskView.java
+++ TaskView.java
@@ -1,8 +1,66 @@
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 public class TaskView {
+    
     public void displaySchedule() {
         System.out.print("Schedule: ");
         /* TODO: Add formatting for schedule display and different
             display types
          */
+    }
+
+    public void displayMenu() {
+        char response = 'a';
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Welcome to CS3560 PSS! Enter a response at the prompt or press CTRL + X + C to quit.");
+            System.out.println();
+            System.out.println("1: View Schedule");
+            System.out.println("2: Edit Schedule");
+            System.out.println("3: Write To File");
+            System.out.println("4: Read From File");
+            System.out.println();
+
+            try {
+                response = sc.next().charAt(0);
+            } catch (NoSuchElementException e) {
+                System.out.println();
+                System.out.println("Goodbye!");
+                break;
+            }
+
+            switch (response) {
+                case '1':
+                    //display schedule
+                    System.out.println();
+                    System.out.println("View Schedule");
+                    break;
+
+                case '2':
+                    //edit schedule
+                    System.out.println();
+                    System.out.println("Edit Schedule");
+                    break;
+
+                case '3':
+                    //write schedule to file
+                    System.out.println();
+                    System.out.println("Write To File");
+                    break;
+
+                case '4':
+                    //read schedule from file
+                    System.out.println();
+                    System.out.println("Read From File");
+                    break;
+
+                default:
+                    System.out.println("Invalid response. Please enter a valid response.");
+                    break;
+            }
+            System.out.println();
+        }
     }
 }
