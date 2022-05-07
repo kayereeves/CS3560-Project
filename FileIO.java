71d1e0312cf9a8249ff0501eda91c56bdbf3e41f
kayereeves <kiannarachelmarie@gmail.com>
2022-05-07 01:46:49 -0700

    created basic main menu functionality

--- FileIO.java
+++ FileIO.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class FileIO {
--- Main.java
+++ Main.java
@@ -1,8 +1,66 @@
 import javax.swing.event.SwingPropertyChangeSupport;
 
-public class Main {
-    public static void main(String[] args)
-        {
+import java.util.NoSuchElementException;
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        menu();
+
+    }
+
+    public static void menu() {
+        int response = 5;
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Welcome to CS3560 PSS! Enter a response at the prompt or press CTRL + X + C to quit.");
+            System.out.println();
+            System.out.println("1: View Schedule");
+            System.out.println("2: Edit Schedule");
+            System.out.println("3: Write To File");
+            System.out.println("4: Read From File");
+            System.out.println();
+
+            try {
+                response = sc.nextInt();
+            }
+            catch (NoSuchElementException e) {
+                System.out.println();
+                System.out.println("Goodbye!");
+                break;
+            }
+
+            switch(response) {
+                case 1:
+                    //display schedule
+                    System.out.println();
+                    System.out.println("View Schedule");
+                    break;
+                
+                case 2:
+                    //edit schedule
+                    System.out.println();
+                    System.out.println("Edit Schedule"); 
+                    break;
+
+                case 3:
+                    //write schedule to file
+                    System.out.println();
+                    System.out.println("Write To File");
+                    break;
+
+                case 4:
+                    //read schedule from file
+                    System.out.println();
+                    System.out.println("Read From File");
+                    break;
+
+                default:
+                    System.out.println("Invalid response. Please enter a valid response.");
+                    break;
+            }
+            System.out.println();
         }
+    }
 }
