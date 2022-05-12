af5e4b9494259f543d588bad7341bcf0795b36ff
nipplelion <piolotorrico@yahoo.com>
2022-05-12 01:42:57 -0700

    Display error if name not found for removing a task

--- TaskModel.java
+++ TaskModel.java
@@ -1,4 +1,7 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
 
 public class TaskModel {
     private List<Task> taskData = new ArrayList<Task>();
@@ -45,71 +48,77 @@ public class TaskModel {
     }
 
     public FileIO getFileIO() {
-    	return this.fileIO;
+        return this.fileIO;
     }
-    
+
     public TaskView getView() {
-    	return this.view;
+        return this.view;
     }
 
     public void removeTask(String taskName) {
-        this.taskData.removeIf(task -> task.getName().equals(taskName));
-
-        System.out.println("Task \"" + taskName + "\" successfully removed.");
+        if (this.taskData.removeIf(task -> task.getName().equals(taskName))) {
+            System.out.println("Task \"" + taskName + "\" successfully removed.");
+        } else {
+            System.out.println("Task \"" + taskName + "\" not found.");
+        }
         System.out.println();
     }
 
     public void createTask(String type, TaskView view) {
 
         Scanner s = view.getScanner();
-        
-        switch(type){
+
+        switch (type) {
             case "transienttask":
-            Object[] transientData = view.retrieveTaskData(type);
-            taskData.add(
-                    new TransientTask(
-                         String.valueOf(transientData[0]),
-                         String.valueOf(transientData[1]),
-                         (Time)transientData[2],
-                         (Time)transientData[3],
-                         (Date)transientData[4]
-                         ));
-            break;
-            case "recurringtask":
-            Object[] recurringData = view.retrieveTaskData(type);
-            taskData.add(
-                        new RecurringTask(
-                            String.valueOf(recurringData[0]),
-                            String.valueOf(recurringData[1]),
-                            (Time)recurringData[2],
-                            (Time)recurringData[3],
-                            (Date)recurringData[4],
-                            (Date)recurringData[5],
-                            (int)recurringData[6]
+                Object[] transientData = view.retrieveTaskData(type);
+                taskData.add(
+                        new TransientTask(
+                                String.valueOf(transientData[0]),
+                                String.valueOf(transientData[1]),
+                                (Time) transientData[2],
+                                (Time) transientData[3],
+                                (Date) transientData[4]
                         ));
-            break;
+                break;
+            case "recurringtask":
+                Object[] recurringData = view.retrieveTaskData(type);
+                taskData.add(
+                        new RecurringTask(
+                                String.valueOf(recurringData[0]),
+                                String.valueOf(recurringData[1]),
+                                (Time) recurringData[2],
+                                (Time) recurringData[3],
+                                (Date) recurringData[4],
+                                (Date) recurringData[5],
+                                (int) recurringData[6]
+                        ));
+                break;
             case "antitask":
-            Object[] antitaskData = view.retrieveTaskData(type);
-            taskData.add(
-                    new AntiTask(
-                        String.valueOf(antitaskData[0]),
-                        String.valueOf(antitaskData[1]),
-                        (Time)antitaskData[2],
-                        (Time)antitaskData[3],
-                        (Date)antitaskData[4]
-                    ));
-            break;
+                Object[] antitaskData = view.retrieveTaskData(type);
+                taskData.add(
+                        new AntiTask(
+                                String.valueOf(antitaskData[0]),
+                                String.valueOf(antitaskData[1]),
+                                (Time) antitaskData[2],
+                                (Time) antitaskData[3],
+                                (Date) antitaskData[4]
+                        ));
+                break;
             default:
-            System.out.println("Could not create task.");
+                System.out.println("Could not create task.");
         }
 
         System.out.println("Task added to schedule.");
         System.out.println();
     }
 
-    public void editTask() {
-        // TODO: Edit task method
-        // taskData.add(task);
+    public void editTask(String taskName) {
+        if (this.taskData.contains(taskName)) {
+
+        } else {
+            System.out.println("Task \"" + taskName + "\" not found.");
+        }
+        System.out.println();
     }
 
     public void writeFile() {
@@ -120,8 +129,7 @@ public class TaskModel {
             try {
                 fileName = this.view.getScanner().next("([0-9]?[a-z]?[A-Z]?)*.json");
                 break;
-            }
-            catch(InputMismatchException e) {
+            } catch (InputMismatchException e) {
                 System.out.print("Input error. Check format and try again: ");
                 this.view.getScanner().next();
             }
@@ -138,8 +146,7 @@ public class TaskModel {
             try {
                 fileName = this.view.getScanner().next("([0-9]?[a-z]?[A-Z]?)*.json");
                 break;
-            }
-            catch(InputMismatchException e) {
+            } catch (InputMismatchException e) {
                 System.out.print("Input error. Check format and try again: ");
                 this.view.getScanner().next();
             }
