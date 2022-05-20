90e44fba057a248fe9fc1910238739fb5857ea1d
DoozyKiddo <52268953+DoozyKiddo@users.noreply.github.com>
2022-05-20 03:08:37 -0700

    Add files via upload

--- FileIO.java
+++ FileIO.java
@@ -26,25 +26,46 @@ public class FileIO {
             // starting at i = 1 gets rid of strange bracket at i = 0
             for(int i = 1; i<allTasks.length; i++){
             	String[] split = allTasks[i].split("[:,]");
+            	// removes whitespace from all tasks
             	for (int j = 0; j < split.length; j++) {
             		split[j] = split[j].replaceAll("\\s", "");
             	}
+            	// ensuring that last blank element gets removed from array, does not apply for last one
+            	if (i != allTasks.length-1) {
+            		String[] temp = new String[split.length-1];
+            		for (int k = 0; k < split.length-1; k++) {
+            			temp[k] = split[k];
+            		}	
+            		split = temp;
+            		// the last one will have two characters at the end that must be removed, so this is done one extra time
+            	} else {
+            		split[split.length-1] = split[split.length-1].substring(0,split[split.length-1].length()-1);
+            	}
+            	split[split.length-1] = split[split.length-1].substring(0,split[split.length-1].length()-1);
             	System.out.println(Arrays.toString(split));
                 taskData.add(split);
                 
             }
-            //System.out.println(taskData.get(1)[3]); //task type
+            // printing out all taskData values
+            for (int i = 0; i < taskData.size(); i++) {
+            	for (int j = 0; j < taskData.get(i).length; j++) {
+            		System.out.println(taskData.get(i)[j]);
+            	}
+            }
 
             //individual data slots;
-             for( int i = 1; i < taskData.size(); i++ ) {
+             for( int i = 0; i < taskData.size(); i++ ) {
 
                   //index 3 holds the task type -- length 14 will be recurring task
                   if(taskData.get(i).length == 14){
-                        createTaskfromFile(tasks, taskData, "recurringtask", i, taskData.get(i).length);
+                        tasks.add(createTaskfromFile(tasks, taskData, "recurringtask", i, taskData.get(i).length));
                   }
-                  //length 11 is transient task
-                  else if(taskData.get(i).length == 11){
-                    createTaskfromFile(tasks, taskData, "transienttask", i, taskData.get(i).length);
+                  //checks if task type is cancellation -- will be antitask
+                  else if(taskData.get(i)[3].equals("Cancellation")){
+                	  tasks.add(createTaskfromFile(tasks, taskData, "antitask", i, taskData.get(i).length));
+                  //everything else is transient by process of elimination
+                  } else {
+                	  tasks.add(createTaskfromFile(tasks, taskData, "transienttask", i, taskData.get(i).length));
                   }
           //    }
 
@@ -128,29 +149,29 @@ public class FileIO {
         return json;
     }
 
-    private void createTaskfromFile(List<Task> tasks, List<String[]> taskData, String task, int index, int length){
+    private Task createTaskfromFile(List<Task> tasks, List<String[]> taskData, String task, int index, int length){
       String stringMinusQuotes = "";
       String combinedValues = "";
       Object[] arr;
 
       for(int i = 1; i<length; i+=2){
         stringMinusQuotes = taskData.get(index)[i];
-        stringMinusQuotes = stringMinusQuotes.substring(2,stringMinusQuotes.length()-1);
+        stringMinusQuotes = stringMinusQuotes.substring(1,stringMinusQuotes.length()-1);
 
         //makes tasks comma delimited in one string so we can break it into an array in the switch statement
-        combinedValues += stringMinusQuotes +",";
+        if (i != length-1) {
+        	combinedValues += stringMinusQuotes +",";
+        } else {
+        	combinedValues += stringMinusQuotes;
+        }
         //System.out.println(combinedValues); //comma delimited values for each task
 
       }
+      System.out.println(combinedValues);
       arr = combinedValues.split(",");
 
 
       switch(task) {
-        case "antitask":
-        //    tasks.add(new AntiTask((String)arr[0], (String[])arr[1], Time.intsToTime(Time.conversion((String)arr[2])),
-        //    Time.intsToTime(Time.conversion((String)arr[3])), Date.intsToDate(Date.conversion((String)arr[4]))));
-        //    break;
-
         case "recurringtask":
         double time = 0;
         String date = "";
@@ -168,51 +189,71 @@ public class FileIO {
         arr[2] = date;
         date = convertDate(((String)arr[5]));
         arr[5] = date;
-
-        //removes extra quotation still in frequency
-        arr[6] = ((String)arr[6]).substring(0, ((String)arr[6]).indexOf("\""));
-
+        
         //the array holds everything we need, printed here so its easier to see what's in it.
         //name type start date startTime endTime endDate frequency
         for(Object dataItem: arr)
           System.out.print(dataItem + " ");
         System.out.println();
         
-            //tasks.add(new RecurringTask((String)arr[0], (String[])arr[1], Time.intsToTime(Time.conversion((String)arr[2])),
-            //Time.intsToTime(Time.conversion((String)arr[3])), Date.intsToDate(Date.conversion((String)arr[4])),
-            //Date.intsToDate(Date.conversion((String)arr[5])), Integer.parseInt((String)arr[6])));
-            break;
+        // shift array elements around to fit task creation
+        // name type startTime endTime start date endDate frequency
+        String[] recurringTypes = {"recurringtask", (String)arr[1]};
+        return new RecurringTask((String)arr[0], recurringTypes, Time.intsToTime(Time.conversion((String)arr[3])), Time.intsToTime(Time.conversion((String)arr[4])), Date.intsToDate(Date.conversion((String)arr[2])), Date.intsToDate(Date.conversion((String)arr[5])), Integer.parseInt((String)arr[6]));
 
         case "transienttask":
-          time = 0;
+        	String[] transientTypes = {"transienttask", (String)arr[1]};
+        	time = 0;
 
-          //removes extra quotation still in duration
-          arr[4] = ((String)arr[4]).substring(0, ((String)arr[4]).indexOf("\""));
+            date = convertDate((String)arr[2]);
+            arr[2] = date;
 
-          date = convertDate(((String)arr[2]));
-          arr[2] = date;
+            time = calculateEndTime(Double.valueOf((String)arr[3]),Double.valueOf((String)arr[4]));
+            //make arr[4] hold endtime instead of duration:
+            arr[4] = correctTimeFormat(String.valueOf(time));
+            System.out.println("End time: " + arr[4]);
+            //now that end time is calculated we can correct start time format:
+            arr[3] = correctTimeFormat(String.valueOf(arr[3]));
+            System.out.println("Start time: " + arr[3]);
 
-          time = calculateEndTime(Double.valueOf((String)arr[3]),Double.valueOf((String)arr[4]));
-          //make arr[4] hold endtime instead of duration:
-          arr[4] = correctTimeFormat(String.valueOf(time));
-          System.out.println("End time: " + arr[4]);
-          //now that end time is calculated we can correct start time format:
-          arr[3] = correctTimeFormat(String.valueOf(arr[3]));
-          System.out.println("Start time: " + arr[3]);
+            //the array holds everything we need, printed here so its easier to see what's in it.
+            //name type startDate startTime endTime
+            for(Object dataItem: arr)
+            System.out.print(dataItem + " ");
+            System.out.println();
+            
+            // shift array elements around to fit task creation
+            // name type startTime endTime startDate
+           return new TransientTask((String)arr[0], transientTypes, Time.intsToTime(Time.conversion((String)arr[3])), Time.intsToTime(Time.conversion((String)arr[4])), Date.intsToDate(Date.conversion((String)arr[2])));
+        case "antitask":
+        	String[] antitaskTypes = {"antitask", (String)arr[1]};
+        	time = 0;
 
-          //the array holds everything we need, printed here so its easier to see what's in it.
-          //name type startDate startTime endTime
-          for(Object dataItem: arr)
-          System.out.print(dataItem + " ");
-          System.out.println();
-        
-           // tasks.add(new TransientTask((String)arr[0], (String[])arr[1], Time.intsToTime(Time.conversion((String)arr[2])),
-            //Time.intsToTime(Time.conversion((String)arr[3])), Date.intsToDate(Date.conversion((String)arr[4]))));
-            break;
+            date = convertDate((String)arr[2]);
+            arr[2] = date;
+
+            time = calculateEndTime(Double.valueOf((String)arr[3]),Double.valueOf((String)arr[4]));
+            //make arr[4] hold endtime instead of duration:
+            arr[4] = correctTimeFormat(String.valueOf(time));
+            System.out.println("End time: " + arr[4]);
+            //now that end time is calculated we can correct start time format:
+            arr[3] = correctTimeFormat(String.valueOf(arr[3]));
+            System.out.println("Start time: " + arr[3]);
+
+            //the array holds everything we need, printed here so its easier to see what's in it.
+            //name type startDate startTime endTime
+            for(Object dataItem: arr)
+            System.out.print(dataItem + " ");
+            System.out.println();
+            
+            // shift array elements around to fit task creation
+            // name type startTime endTime startDate
+           return new AntiTask((String)arr[0], antitaskTypes, Time.intsToTime(Time.conversion((String)arr[3])), Time.intsToTime(Time.conversion((String)arr[4])), Date.intsToDate(Date.conversion((String)arr[2])));
 
         default:
             break;
     }
+	return null;
     }
 
     private double calculateEndTime(double startTime, double duration){
